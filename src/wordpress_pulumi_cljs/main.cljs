a93a89d2c7662df0d273a6eb99c9dfe300c75bc9
Luke VanderHart <luke@vanderhart.net>
2021-06-15 12:52:30 -0400

    tasks starting successfully

--- src/wordpress_pulumi_cljs/main.cljs
+++ src/wordpress_pulumi_cljs/main.cljs
@@ -3,6 +3,7 @@
             ["@pulumi/aws" :as aws]
             [pulumi-cljs.core :as p]
             [pulumi-cljs.aws :as aws-utils]
+            [pulumi-cljs.aws.ecs :as ecs]
             [clojure.walk :as walk]))
 
 ; https://aws.amazon.com/blogs/containers/running-wordpress-amazon-ecs-fargate-ecs/
@@ -10,87 +11,107 @@
 (defn- efs
   "Construct EFS filesystem & related resources"
   [provider]
-  (let [efs (p/resource aws/efs.FileSystem "filesystem" provider
+  (let [efs (p/resource aws/efs.FileSystem (p/id) provider
               {:encrypted true
                :throughputMode "provisioned"
                :provisionedThroughputInMibps 1}
               {:provider provider})
-        sg (p/resource aws/ec2.SecurityGroup "filesystem" efs
+        sg (p/resource aws/ec2.SecurityGroup (p/id "filesystem") efs
              {:vpcId (p/cfg "vpc")
-              :description "Filesystem mounts for EFS backing WordPress"})]
-    (p/resource aws/efs.AccessPoint "access-point" efs
-      {:fileSystemId (:id efs)
-       :posixUser {:uid 1000 :gid 1000}
-       :rootDirectory {:creationInfo {:ownerGid 1000
-                                      :ownerUid 1000
-                                      :permissions "0777"}
-                       :path "/bitnami"}})
+              :description "Filesystem mounts for EFS backing WordPress"})
+        ap (p/resource aws/efs.AccessPoint (p/id) efs
+             {:fileSystemId (:id efs)
+              :posixUser {:uid 1000 :gid 1000}
+              :rootDirectory {:creationInfo {:ownerGid 1000
+                                             :ownerUid 1000
+                                             :permissions "0777"}
+                              :path "/bitnami"}})]
+
     (doseq [subnet (p/cfg-obj "private-subnets")]
-      (p/resource aws/efs.MountTarget (str "mt-" subnet) efs
+      (p/resource aws/efs.MountTarget (p/id subnet) efs
         {:fileSystemId (:id efs)
          :subnetId subnet
          :securityGroups [(:id sg)]}))
     {:filesystem efs
-     :security-group sg}))
+     :security-group sg
+     :access-point ap}))
 
 (defn- db
   "Construct RDS instance & related resources"
   [provider]
-  (let [sg (p/resource aws/ec2.SecurityGroup "wordpress-db" provider
+  (let [sg (p/resource aws/ec2.SecurityGroup (p/id "db") provider
              {:vpcId (p/cfg "vpc")
               :description "RDS database access for WordPress"}
              {:provider provider})
-        subnet-group (p/resource aws/rds.SubnetGroup "subnet-group" sg
-                       {:namePrefix "wordpress-"
+        subnet-group (p/resource aws/rds.SubnetGroup (p/id) sg
+                       {:namePrefix (str (p/id) "-")
                         :subnetIds (p/cfg-obj "private-subnets")}
                        {:provider provider})
         pw (aws-utils/ssm-password sg "db-admin")
-        rds (p/resource aws/rds.Cluster "wordpress" sg
-              {:clusterIdentifier (str (pulumi/getProject) "-" (pulumi/getStack))
-               :engine "aurora-mysql"
-               :engineMode "serverless"
-               :engineVersion "5.7.mysql_aurora.2.07.1"
-               :masterUsername "admin"
-               :masterPassword (:value pw)
-               :backupRetentionPeriod 35
-               :dbSubnetGroupName (:name subnet-group)
-               :databaseName "wordpress"
-               :port 3306
-               :vpcSecurityGroupIds [(:id sg)]
-               :skipFinalSnapshot (not (p/cfg "protect"))})]
-    {:db rds
+        cluster (p/resource aws/rds.Cluster (p/id) sg
+                  {:clusterIdentifier (str (pulumi/getProject) "-" (pulumi/getStack))
+                   :engine "aurora-mysql"
+                   :engineMode "serverless"
+                   :engineVersion "5.7.mysql_aurora.2.07.1"
+                   :masterUsername "admin"
+                   :masterPassword (:value pw)
+                   :backupRetentionPeriod 35
+                   :dbSubnetGroupName (:name subnet-group)
+                   :databaseName "wordpress"
+                   :port 3306
+                   :vpcSecurityGroupIds [(:id sg)]
+                   :skipFinalSnapshot (not (p/cfg "protect"))})]
+    {:cluster cluster
+     :password-param pw
      :security-group sg}))
 
-(defn- lb
-  "Construct load balancer & related resources"
-  [provider]
-  (let [sg (p/resource aws/ec2.SecurityGroup "wordpress-alb" provider
-             {:vpcId (p/cfg "vpc")
-              :description "Public access to ALB"
-              :ingress [{:description "Public HTTP access"
-                         :protocol "tcp"
-                         :fromPort 80
-                         :toPort 80
-                         :cidrBlocks ["0.0.0.0/0"]}]}
-             {:provider provider})
-        alb (p/resource aws/lb.LoadBalancer "wordpress" sg
-              {:loadBalancerType "application"
-               :securityGroups [(:id sg)]
-               :subnets (p/cfg-obj "public-subnets")})
-        target-group (p/resource aws/lb.TargetGroup "wordpress" alb
-                       {:namePrefix "wp-"
-                        :port 8080
-                        :healthCheckPort 8080
-                        :protocol "HTTP"
-                        :targetType "ip"
-                        :vpcId (p/cfg "vpc")})
-        listener (p/resource aws/lb.Listener "wordpress" alb
-                   {:loadBalancerArn (:arn alb)
-                    :port 80
-                    :defaultActions [{:type "forward"
-                                      :targetGroupArn (:arn target-group)}]})]
-    {:alb alb
-     :target-group target-group}))
+(defn- ecs
+  "Create the ECS cluster, task and associated resources"
+  [provider efs db]
+  (let [container-definition {:name (p/id)
+                              :image "bitnami/wordpress"
+                              :portMappings [{:protocol "tcp" :containerPort 8080}]
+                              :essential true
+                              :mountPoints [{:containerPath "/bitnami/wordpress"
+                                             :sourceVolume "wordpress"}]
+                              :environment [{:name "MARIADB_HOST" :value (-> db :cluster :endpoint)}
+                                            {:name "WORDPRESS_DATABASE_USER" :value (-> db :cluster :masterUsername)}
+                                            {:name "WORDPRESS_DATABASE_NAME" :value (-> db :cluster :databaseName)}
+                                            {:name "PHP_MEMORY_LIMIT" :value "512M"}
+                                            {:name "enabled" :value "false"}
+                                            {:name "ALLOW_EMPTY_PASSWORD" :value "yes"}]
+                              :secrets [{:name "WORDPRESS_DATABASE_PASSWORD"
+                                         :valueFrom (-> db :password-param :arn)}]}
+        volume-configuration {:name "wordpress"
+                              :efsVolumeConfiguration
+                              {:fileSystemId (-> efs :filesystem :id)
+                               :transitEncryption "ENABLED"
+                               :authorizationConfig {:accessPointId (-> efs :access-point :id)}}}
+        cluster (p/resource aws/ecs.Cluster (p/id) provider
+                  {:settings [{:name "containerInsights"
+                               :value "enabled"}]}
+                  {:provider provider})
+        iam-stmts [(aws-utils/allow-stmt (-> db :password-param :arn)
+                     ["ssm:GetParameters"])]]
+    (let [service (ecs/service provider provider (p/id) {:vpc-id (p/cfg "vpc")
+                                                         :container-port 8080
+                                                         :cluster-id (:id cluster)
+                                                         :lb {:ingress-cidrs (p/cfg-obj "ingress-cidrs")
+                                                              :subnets (p/cfg-obj "public-subnets")}
+                                                         :task {:container-definitions [container-definition]
+                                                                :cpu 1024
+                                                                :memory 3072
+                                                                :volumes [volume-configuration]
+                                                                :iam-statements iam-stmts
+                                                                :subnets (p/cfg-obj "private-subnets")}})]
+      (p/resource aws/ec2.SecurityGroupRule (p/id "fs-ingress") (:security-group efs)
+        {:type "ingress"
+         :protocol "tcp"
+         :fromPort 2049
+         :toPort 2049
+         :securityGroupId (-> efs :security-group :id)
+         :sourceSecurityGroupId (-> service :security-group :id)})
+      service)))
 
 (defn ^:export stack
   "Create the Pulumi stack, returning its outputs"
@@ -98,8 +119,10 @@
   (let [provider (p/resource aws/Provider. "provider" nil
                    {:region (.require (p/load-cfg "aws") "region")
                     :defaultTags {:tags {"pulumi:stack" (pulumi/getStack)
-                                         "pulumi:project" (pulumi/getProject)}}})]
-    (p/prepare-output {:efs (efs provider)
-                       :db (db provider)
-                       :lb (lb provider)
-                       })))
+                                         "pulumi:project" (pulumi/getProject)}}})
+        efs (efs provider)
+        db (db provider)
+        ]
+    (p/prepare-output {:efs efs
+                       :db db
+                       :ecs (ecs provider efs db)})))
